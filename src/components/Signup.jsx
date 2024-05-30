28e94dd56bd8cb76b116d4cf6f477e927d763c06
arunkumar0398 <32316065+arunkumar0398@users.noreply.github.com>
2024-05-30 20:22:54 +0530

    fix: update styling for signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,25 +5,27 @@ export const SignUp = function() {
 
     return(
         <div>
-            <h1>Sign up</h1>
+            <h1 className="heading">Sign up</h1>
 
             <form>
-                <div className="">
+                <div className="inputSection">
                     <label htmlFor="name" className="inputLabel">Name</label>
                     <input type="text" name="name" />
                 </div>
 
-                <div className="">
+                <div className="inputSection">
                     <label htmlFor="emailId" className="inputLabel">Email ID</label>
                     <input type="email" name="emailId" />
                 </div>
 
-                <div className="">
+                <div className="inputSection">
                     <label htmlFor="pwd" className="inputLabel">Password</label>
                     <input type="password" name="pwd" />
                 </div>
 
-                <Link to="/dashboard" state={{userSignUp: true}}>Sign Up</Link>
+                <div className="block-link">
+                    <Link to="/dashboard" className="signUp-link" state={{userSignUp: true}}>Sign Up</Link>
+                </div>
             </form>
         </div>
      )
